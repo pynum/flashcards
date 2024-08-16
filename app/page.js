1f3c8a181bb5d7721c300a37ccca023cd5c7b439
System Administrator <root@Tamzids-MacBook-Pro.local>
2024-08-15 22:55:13 -0400

    first commit

--- app/page.js
+++ app/page.js
@@ -9,58 +9,6 @@ import EmojiObjectsIcon from '@mui/icons-material/EmojiObjects';
 import SchoolIcon from '@mui/icons-material/School';
 import PsychologyIcon from '@mui/icons-material/Psychology';
 
-const {VertexAI} = require('@google-cloud/vertexai');
-
-// Initialize Vertex with your Cloud project and location
-const vertex_ai = new VertexAI({project: 'atlas-journal', location: 'us-central1'});
-const model = 'gemini-1.5-pro-001';
-
-// Instantiate the models
-const generativeModel = vertex_ai.preview.getGenerativeModel({
-  model: model,
-  generationConfig: {
-    'maxOutputTokens': 8192,
-    'temperature': 1,
-    'topP': 0.95,
-  },
-  safetySettings: [
-    {
-        'category': 'HARM_CATEGORY_HATE_SPEECH',
-        'threshold': 'BLOCK_MEDIUM_AND_ABOVE'
-    },
-    {
-        'category': 'HARM_CATEGORY_DANGEROUS_CONTENT',
-        'threshold': 'BLOCK_MEDIUM_AND_ABOVE'
-    },
-    {
-        'category': 'HARM_CATEGORY_SEXUALLY_EXPLICIT',
-        'threshold': 'BLOCK_MEDIUM_AND_ABOVE'
-    },
-    {
-        'category': 'HARM_CATEGORY_HARASSMENT',
-        'threshold': 'BLOCK_MEDIUM_AND_ABOVE'
-    }
-  ],
-});
-
-
-async function generateContent() {
-  const req = {
-    contents: [
-    ],
-  };
-
-  const streamingResp = await generativeModel.generateContentStream(req);
-
-  for await (const item of streamingResp.stream) {
-    process.stdout.write('stream chunk: ' + JSON.stringify(item) + '\n');
-  }
-
-  process.stdout.write('aggregated response: ' + JSON.stringify(await streamingResp.response));
-}
-
-generateContent();
-
 const API_URL = 'https://api.openai.com/v1/chat/completions';
 
 // Define a default theme
